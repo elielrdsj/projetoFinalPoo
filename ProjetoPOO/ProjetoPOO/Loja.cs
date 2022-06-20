c4fc3eafd5c22baeeb775ee7ffa6d0e5dc2be6df
elielrdsj <79999508+elielrdsj@users.noreply.github.com>
2022-06-20 18:31:17 -0300

    Adicionar os arquivos bases do projeto em c#

---
+++ ProjetoPOO/ProjetoPOO/Loja.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoPOO
+{
+    internal class Loja
+    {
+        public int numeroDaloja;
+        public string numeroTelefone;
+        public string email;
+        public string endereco;
+    }
+}
