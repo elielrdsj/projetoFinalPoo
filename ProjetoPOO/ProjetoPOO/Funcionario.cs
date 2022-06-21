17a152539332c2ef99db1629797d82cb7a1758af
elielrdsj <79999508+elielrdsj@users.noreply.github.com>
2022-06-21 18:42:55 -0300

    Adicionar classes restantes

---
+++ ProjetoPOO/ProjetoPOO/Funcionario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoPOO
+{
+    internal class Funcionario : Pessoa {
+
+        public string funcao;
+        public Gerente? gerente;
+        bool estaDeFerias = false;
+        public bool EstaDeFerias
+        {
+            get { return estaDeFerias; }
+
+            set { estaDeFerias = value; }
+        }
+
+    }
+}
