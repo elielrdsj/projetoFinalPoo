ac27916344f14ef0343eff52c663b584a191b824
elielrdsj <79999508+elielrdsj@users.noreply.github.com>
2022-06-21 17:04:22 -0300

    Add files via upload

---
+++ ProjetoPOO/ProjetoPOO/Produto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoPOO
+{
+    public class Produto
+    {
+        public long codigoBarras;
+        public string descricao;
+        public double preco;
+        public double estoque;
+        public int unidadeDeMedida;
+        public Loja loja;
+
+        public void AlteraQuant(double quant)
+        {
+            estoque += quant;
+        }
+        public void AlteraPreco (double novoPreco)
+        {
+            preco = novoPreco;
+        }
+    }
+}
