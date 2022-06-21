342a119ec556e239b3c4d2ff3290a2da21d7274b
unknown <dudusegui@gmail.com>
2022-06-21 16:46:00 -0300

    Adicionando classe cliente

---
+++ ProjetoPOO/ProjetoPOO/Cliente.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoPOO
+{
+    public class Cliente
+    {
+        public string? nomeCliente;
+        private long ncpf;
+        private string enederecoCliente;
+        public bool temCartaoLoja = false;
+
+        public Cliente(string nome, long cpf, string endereco)
+        {
+            this.nomeCliente = nome;
+            this.ncpf = cpf;
+            this.enederecoCliente = endereco;
+        }
+
+        public void TemCartao()
+        {
+            temCartaoLoja = true;
+        }
+    }
+}
