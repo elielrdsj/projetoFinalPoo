d597d445af12ad6d5b80f4b9fbaf19523236b099
Fábio Garcia Rios <fabiogarcia45@gmail.com>
2022-06-21 17:37:40 -0300

    Add files via upload

---
+++ ProjetoPOO/ProjetoFinalPOObase/ProjetoFinalPOObase/Loja.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoFinalPOObase
+{
+    public class Loja
+    {
+        public byte? numeroDaloja;
+        public string? numeroTelefone;
+        public string? email;
+        public string? endereco;
+
+    }
+}
