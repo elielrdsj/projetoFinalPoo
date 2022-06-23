3765f67ced2960302db5fa34b3289a1442269b7f
elielrdsj <79999508+elielrdsj@users.noreply.github.com>
2022-06-23 11:45:44 -0300

    Versão final

---
+++ ProjetoPOO/ProjetoPOO/Gerente.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoPOO
+{
+    internal class Gerente : Pessoa
+    {
+
+        public Loja? loja;
+        double salarioFinal;
+
+        public void Ferias(Funcionario funcionario)
+        {
+
+            funcionario = new Funcionario();
+
+            if (funcionario.EstaDeFerias == false)
+            {
+
+                funcionario.EstaDeFerias = true;
+                Console.WriteLine("Funcionário está de férias!");
+
+            }
+            else
+            {
+
+                funcionario.EstaDeFerias = false;
+                Console.WriteLine("Funcionário voltou da férias!");
+            }
+
+        }
+
+        public void AlteraSalario(double salario)
+        {
+
+            salarioFinal = Salario;
+
+            Console.Write("Digite a porcentagem de aumento: ");
+            double aumento = double.Parse(Console.ReadLine());
+
+            salario += ((salario * aumento) / 100);
+            Salario = salario;
+            Console.WriteLine("Novo salario: " + salario);
+            salarioFinal = salario;
+
+        }
+
+        void CalculaSalario()
+        {
+
+            Console.WriteLine("O salário é: " + salarioFinal);
+
+        }
+
+    }
+}
--- ProjetoPOO/ProjetoPOO/Produto.cs
+++ ProjetoPOO/ProjetoPOO/Produto.cs
@@ -9,11 +9,11 @@ namespace ProjetoPOO
     public class Produto
     {
         public long codigoBarras;
-        public string descricao;
+        public string? descricao;
         public double preco;
         public double estoque;
-        public int unidadeDeMedida;
-        public Loja loja;
+        public string? unidadeDeMedida;
+        public Loja? loja;
 
         public void AlteraQuant(double quant)
         {
@@ -22,6 +22,14 @@ namespace ProjetoPOO
         public void AlteraPreco (double novoPreco)
         {
             preco = novoPreco;
+        }
+
+        public void ImprimeDados()
+        {
+            Console.WriteLine("Descrição: " + this.descricao);
+            Console.WriteLine("Preço: R$" + this.preco);
+            Console.WriteLine("Unidade de Medida: " + this.unidadeDeMedida);
+            Console.WriteLine("Quantidade em estoque: " + this.estoque);
         }
     }
 }
